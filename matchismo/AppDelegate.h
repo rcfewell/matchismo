10f530b98961d80636492b164c363c735d79fba0
Julia Foster <juliafoster@julia-fosters-macbook-pro.local>
2016-03-14 09:57:47 -0700

    Initial Commit

---
+++ matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  matchismo
+//
+//  Created by Julia Foster on 3/14/16.
+//  Copyright © 2016 Riley Fewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  matchismo
+//
+//  Created by Julia Foster on 3/14/16.
+//  Copyright © 2016 Riley Fewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
